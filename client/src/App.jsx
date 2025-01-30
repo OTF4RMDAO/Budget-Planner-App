4b6f2f5c8df903cea2fb917a10cf2c8346b929f3
xxxfairley@gmail.com <xxxfairley@gmail.com>
2025-01-30 12:51:27 +0100

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard.js';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
